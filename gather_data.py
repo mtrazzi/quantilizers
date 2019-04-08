7889c851ceff14f911c5e89527069fec4cce4390
Michael Trazzi <michael.trazzi@gmail.com>
2019-04-08 12:49:36 +0100

    added data gathering. Still need to debug

---
+++ gather_data.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+from play import play_1d
+import itertools as it
+import argparse
+from datetime import datetime
+
+def main(env_name, filename):
+    if filename is None:
+        filename = "traj.npz"
+    env = gym.make("Hopper-v2")
+    lr = [('a',-1.),('d',1.), ('',0)]
+    ud = [('s',-1.),('w',1.), ('',0)]
+    fb = [('k',-1.),('j',1.), ('',0)]
+    keys = {((k1+k2+k3)):np.array([m1,m2,m3]) for (k1,m1),(k2,m2),(k3,m3) in it.product(lr, ud, fb)}
+    keys = {tuple(sorted((ord(k) for k in key))):val for key,val in keys.items()}
+    keys[(0,)] = np.array([0.,0.,0])
+
+    play_1d(env, keys_to_action=keys, save=True, filename='log/{}/{}'.format(env_name, filename))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env_id", default="Hopper-v2")
+    parser.add_argument("--filename", default=None)
+    args = parser.parse_args()
+    main(args.env_id, datetime.now().strftime("%m%d-%H%M%S"))
--- play.py
+++ play.py
@@ -6,17 +6,8 @@ import matplotlib
 import numpy as np
 import os
 import os.path as osp
-try:
-    matplotlib.use('GTK3Agg')
-    import matplotlib.pyplot as plt
-except Exception:
-    pass
-
-
 import pyglet.window as pw
-
 from collections import deque
-from pygame.locals import HWSURFACE, DOUBLEBUF, RESIZABLE, VIDEORESIZE
 from threading import Thread
 
 def display_arr(screen, arr, video_size, transpose):
@@ -27,62 +18,9 @@ def display_arr(screen, arr, video_size, transpose):
     screen.blit(pyg_img, (0,0))
 
 def play_1d(env, fps=30, zoom=None, callback=None, keys_to_action=None, save=False, filename='traj.npz'):
-    """Allows one to play the game using keyboard.
-
-    To simply play the game use:
-
-        play(gym.make("Pong-v3"))
-
-    Above code works also if env is wrapped, so it's particularly useful in
-    verifying that the frame-level preprocessing does not render the game
-    unplayable.
-
-    If you wish to plot real time statistics as you play, you can use
-    gym.utils.play.PlayPlot. Here's a sample code for plotting the reward
-    for last 5 second of gameplay.
-
-        def callback(obs_t, obs_tp1, rew, done, info):
-            return [rew,]
-        env_plotter = EnvPlotter(callback, 30 * 5, ["reward"])
-
-        env = gym.make("Pong-v3")
-        play(env, callback=env_plotter.callback)
-
-
-    Arguments
-    ---------
-    env: gym.Env
-        Environment to use for playing.
-    fps: int
-        Maximum number of steps of the environment to execute every second.
-        Defaults to 30.
-    zoom: float
-        Make screen edge this many times bigger
-    callback: lambda or None
-        Callback if a callback is provided it will be executed after
-        every step. It takes the following input:
-            obs_t: observation before performing action
-            obs_tp1: observation after performing action
-            action: action that was executed
-            rew: reward that was received
-            done: whether the environment is done or not
-            info: debug info
-    keys_to_action: dict: tuple(int) -> int or None
-        Mapping from keys pressed to action performed.
-        For example if pressed 'w' and space at the same time is supposed
-        to trigger action number 2 then key_to_action dict would look like this:
-
-            {
-                # ...
-                sorted(ord('w'), ord(' ')) -> 2
-                # ...
-            }
-        If None, default key_to_action mapping for that env is used, if provided.
-    """
 
     obs_s = env.observation_space
     assert type(obs_s) == gym.spaces.box.Box
-    #assert len(obs_s.shape) == 2 or (len(obs_s.shape) == 3 and obs_s.shape[2] in [1,3])
 
     if keys_to_action is None:
         if hasattr(env, 'get_keys_to_action'):
@@ -93,18 +31,11 @@ def play_1d(env, fps=30, zoom=None, callback=None, keys_to_action=None, save=Fal
             assert False, env.spec.id + " does not have explicit key to action mapping, " + \
                           "please specify one manually"
     relevant_keys = set(sum(map(list, keys_to_action.keys()),[]))
-
-
-    #if zoom is not None:
-    #    video_size = int(video_size[0] * zoom), int(video_size[1] * zoom)
-
     pressed_keys = []
     running = True
     env_done = True
     #screen requires arg of length 2 (restricted to 2d games)
-    #video_size = env.observation_space.shape[0], env.observation_space[1]
     screen = pygame.display.set_mode((300,300))
-    #screen = pygame.display.set_mode(video_size)
     clock = pygame.time.Clock()
 
     if save:
@@ -135,11 +66,7 @@ def play_1d(env, fps=30, zoom=None, callback=None, keys_to_action=None, save=Fal
         if obs is not None:
             if len(obs.shape) == 2:
                 obs = obs[:, :, None]
-            #if obs.shape[2] == 1:
-            #    obs = obs.repeat(3, axis=2)
-            #display_arr(screen, obs, video_size=video_size)
             env.render()
-            #print(obs)
 
         # process pygame events
         for event in pygame.event.get():
@@ -190,51 +117,10 @@ def play_1d(env, fps=30, zoom=None, callback=None, keys_to_action=None, save=Fal
         data_save['done'] = pad2d(data_save['done'], (n_eps, ep_length), val=True)
         data_save['ep_rets'] = np.array([np.sum(rews) for rews in data_save['rews']])
         data_save['rews'] = pad2d(data_save['rews'], (n_eps, ep_length))
-        #data_save['acs'] = pad2d(data_save['acs'], (n_eps, ep_length)) #for 1d task
         data_save['acs'] = pad3d(data_save['acs'], (n_eps, ep_length, len(data_save['acs'][0][0]))) #for mujoco
         directory = osp.dirname(filename) 
         if not osp.exists(directory):
             os.makedirs(directory)
-        #import ipdb; ipdb.set_trace()
         np.savez(filename, **data_save)
 
     pygame.quit()
-
-class PlayPlot(object):
-    def __init__(self, callback, horizon_timesteps, plot_names):
-        self.data_callback = callback
-        self.horizon_timesteps = horizon_timesteps
-        self.plot_names = plot_names
-
-        num_plots = len(self.plot_names)
-        self.fig, self.ax = plt.subplots(num_plots)
-        if num_plots == 1:
-            self.ax = [self.ax]
-        for axis, name in zip(self.ax, plot_names):
-            axis.set_title(name)
-        self.t = 0
-        self.cur_plot = [None for _ in range(num_plots)]
-        self.data     = [deque(maxlen=horizon_timesteps) for _ in range(num_plots)]
-
-def callback(self, obs_t, obs_tp1, action, rew, done, info):
-    print("entered callback")
-    return [rew,]
-    # points = self.data_callback(obs_t, obs_tp1, action, rew, done, info)
-    # for point, data_series in zip(points, self.data):
-    #     data_series.append(point)
-    # self.t += 1
-
-    # xmin, xmax = max(0, self.t - self.horizon_timesteps), self.t
-
-    # for i, plot in enumerate(self.cur_plot):
-    #     if plot is not None:
-    #         plot.remove()
-    #     self.cur_plot[i] = self.ax[i].scatter(range(xmin, xmax), list(self.data[i]))
-    #     self.ax[i].set_xlim(xmin, xmax)
-    # plt.pause(0.000001)
-
-
-if __name__ == '__main__':
-    env = gym.make("Hopper-v2")
-    plotter = PlayPlot(callback, 30 * 5, ["reward"])
-    play_1d(env, callback=plotter.data_callback, zoom=4, fps=60)
