5117ba9a6f055a6841b1b690a7bf3e7baaab10e7
mtrazzi <michael.trazzi@gmail.com>
2019-04-12 17:42:20 +0100

    fixed load_data.sh to be more modular

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,7 +1,10 @@
-wget https://www.dropbox.com/sh/5p2f46s23eum86s/AADhtxW5WM2P1b2w82rn7xyla?dl=0
 FILENAME="AADhtxW5WM2P1b2w82rn7xyla?dl=0" 
+if [ ! -f $FILENAME ]; then
+	echo "ZIP file not found, will download it from dropbox"
+	wget https://www.dropbox.com/sh/5p2f46s23eum86s/AADhtxW5WM2P1b2w82rn7xyla?dl=0
+fi
 LOGNAME="log.zip"
-TARGET_REPO="log"
-mv $FILENAME $LOGNAME
+TARGET_REPO="../log"
+/bin/mv $FILENAME* $LOGNAME
 unzip $LOGNAME -d $TARGET_REPO
 rm $LOGNAME 
